2d204f811c0b339d45533d443720599ab141afc2
卢潇 <xiao.lu@magicwindow.cn>
2018-11-26 22:15:02 +0800

    update

--- pages/bookrack/bookrack.js
+++ pages/bookrack/bookrack.js
@@ -1,85 +1,62 @@
-// pages/bookrack/bookrack.js
+var app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-    currentTab: 0
+    winHeight: "", //窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    expertList: [{ //假数据
+      img: "avatar.png",
+      name: "欢顔",
+      tag: "知名情感博主",
+      answer: 134,
+      listen: 2234
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function(options) {
-
-  },
-
-  //滑动切换
-  swiperTab: function(e) {
-    var that = this;
-    that.setData({
+  // 滚动切换标签样式
+  switchTab: function(e) {
+    this.setData({
       currentTab: e.detail.current
     });
+    this.checkCor();
   },
-  //点击切换
-  clickTab: function(e) {
-    var that = this;
-    if (this.data.currentTab === e.target.dataset.current) {
+  // 点击标题切换当前页时改变样式
+  swichNav: function(e) {
+    var cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) {
       return false;
     } else {
-      that.setData({
-        currentTab: e.target.dataset.current
+      this.setData({
+        currentTab: cur
       })
     }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
-
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function() {
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
+      })
+    } else {
+      this.setData({
+        scrollLeft: 0
+      })
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
+  onLoad: function() {
+    var that = this;
+    //  高度自适应
+    wx.getSystemInfo({
+      success: function(res) {
+        var clientHeight = res.windowHeight,
+          clientWidth = res.windowWidth,
+          rpxR = 750 / clientWidth;
+        var calc = clientHeight * rpxR;
+        console.log(calc)
+        that.setData({
+          winHeight: calc
+        });
+      }
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function() {
-
-  }
+  footerTap: app.footerTap
 })
