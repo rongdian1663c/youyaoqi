fa425a6d635ddc888c6da7748530bc1f45d69c78
xiao.lu <xiao.lu@magicwindow.cn>
2018-12-03 11:02:18 +0800

    Tab撑满,居中

--- http/api.js
+++ http/api.js
@@ -5,7 +5,8 @@ export default {
     "url": '/v3/appV3_3/ios/phone/comic/getDetectList'
   }),
   getDetectUpdataList: (day=1) => http.get({
-    "url": '/v3/appV3_3/ios/phone/list/todayRecommendList?page=0&day='+ (day+1)
+    "url": '/v3/appV3_3/ios/phone/list/todayRecommendList?page=0&day='+ (day+1),
+    "showLoading":false
   }),
   getChapterList: () => http.get({
     "url": '/v3/appV3_3/ios/phone/comic/detail_realtime?version=4.3.12&comicid=' + 172620
