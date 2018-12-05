07cf7cbc6f19362ae7691df6de688305be099402
lxh <351510443@qq.com>
2018-12-05 22:02:21 +0800

    commit

--- pages/particulars/particulars.js
+++ pages/particulars/particulars.js
@@ -6,89 +6,100 @@ Page({
     comicObj: null,
     comicList: [],
     comicDetaiList: null,
+    collect: null,
+    comicCatalog: []
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  // onLoad: function (options) {
-  //   this.setData({
-  //     comicObj: JSON.parse(options.comic)
-  //   })
-  // },
-  onLoad: function (options) {
+  fiexdAmound: function(num) {
+    let amount = num.toString();
+    let list = amount.split(".");
+    if (list.length === 1) {
+      return amount + '.00';
+    } else {
+      if (list[1].length === 1) {
+        return amount + "0"
+      } else {
+        return amount;
+      }
+    }
+  },
+
+  onLoad: function(options) {
     this.setData({
       comicObj: JSON.parse(options.comic)
-      
+
     })
+
     api.getComicList(this.data.comicObj.comicId)
       .then((res) => {
         this.setData({
           comicList: res.comic,
-
+          comicCatalog: res.chapter_list,
         })
       }),
       api.getComicDetailsList(this.data.comicObj.comicId)
-        .then((res) => {
+      .then((res) => {
+        var num = (res.comic.favorite_total) / 10000;
           this.setData({
             comicDetaiList: res.comic,
-
+            collect: num.toFixed(2)
           })
-        })
+      })
 
-      
   },
 
 
-
+  // var num : this.data.comicDetaiList.favorite_total,
+  // collect: num.toFixed(2),
 
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
+
 })
